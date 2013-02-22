947957161d20155ed9c5be05b822f02c96611912
jkennaly <jkennaly@gmail.com>
2013-02-22 10:41:18 -0800

    beginning to add display for live ratings

---
+++ includes/content/blocks/liveranked.php
@@ -0,0 +1,12 @@
+<?php
+
+/* This block displays live ratings and comments for current band
+*  
+*/
+If(!empty$band){
+
+echo "<p><ul><div id=\"liveratings\">";
+echo "</p></ul><br /></div><!-- End #liveratings -->";
+
+}
+?>
