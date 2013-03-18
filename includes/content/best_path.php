4d6c8f2b4c750dca4fcd93b4991ed3f6f82155fd
jkennaly <jkennaly@gmail.com>
2013-03-18 04:09:46 -0400

    upd

--- includes/content/best_path.php
+++ includes/content/best_path.php
@@ -205,7 +205,7 @@ for ($k=$fest_start_time_sec;$k<$fest_end_time_sec;$k=$k+300) {
 		If (!empty($v) && ($looking ==1 || $moving ==1)) {
 			If($looking == 1) {
 					$looked = "Looked";
-				If($v['score'] > $currentbest['score'] && $v['score'] > $target['score'] && $v['name'] != $currentbest['name']) {				
+				If(($v['score'] > $currentbest['score'] ) && ($v['score'] > $target['score'] ) && ( $v['name'] != $currentbest['name'])) {				
 					$target = $v;
 					$travelling=1;
 				}
