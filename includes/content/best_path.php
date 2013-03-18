67c6c1ab2eec6be804201169c4934bf13c45d6d2
jkennaly <jkennaly@gmail.com>
2013-03-17 23:02:03 -0400

    Added status to best path

--- includes/content/best_path.php
+++ includes/content/best_path.php
@@ -190,10 +190,12 @@ echo "<tr><th>Best Path (min 20 min)</th>";
 for ($k=$fest_start_time_sec;$k<$fest_end_time_sec;$k=$k+300) {
 	If(!isset($currentstage)) {
 		$secondpass[$k] = $firstpass[$k];
+		$status="First show of the day";
 //		$nextchecktime=$k+900;
 		
 	} else {
 		$secondpass[$k] = $firstpass[$k];
+		$status="Still rockin'";
 //		$nextchecktime=$k+900;
 		
 	}
@@ -201,7 +203,7 @@ for ($k=$fest_start_time_sec;$k<$fest_end_time_sec;$k=$k+300) {
 		$currentbest = $secondpass[$k];
 		$currentstage = $currentbest['stage'];
 //		$k = $nextchecktime;
-		echo "<td class=\"rating".$currentbest['rating']."\">".$currentbest['name']."<br />at ".getSname($main, $currentbest['stage'])."<br />".$currentbest['score']."</td>";
+		echo "<td class=\"rating".$currentbest['rating']."\">".$currentbest['name']."<br />at ".getSname($main, $currentbest['stage'])."<br />".$status."<br />".$currentbest['score']."</td>";
 	} else echo "<td></td>";
 }
 
