62f21e5d3ebb4d93f182e3857c5f17aff03e6b2d
jkennaly <jkennaly@gmail.com>
2013-03-21 10:55:31 -0400

    fixed error in filter functions

--- includes/content/blocks/filter_functions.php
+++ includes/content/blocks/filter_functions.php
@@ -13,48 +13,56 @@ return $passes;
 function ExternalIncludeFilter($field, $table, $ext_target, $ext_table, $ext_key, $ext_value, $mysql_link){
 	$query="select $ext_target from $ext_table where $ext_key='$ext_value'";
 	$result = mysql_query($query, $mysql_link);
+If(mysql_num_rows($result)>0){
 	while($row = mysql_fetch_array($result)) {
 		If (!isset($first_pass_complete)) $temp = "(".$table.".".$field."='".$row[$ext_target]."'";
 		If (isset($first_pass_complete)) $temp .= " OR ".$table.".".$field."='".$row[$ext_target]."'";
 		$first_pass_complete = 1;
 	}
 $passes = $temp.")";
+} else $passes = "";
 return $passes;
 }
 
 function ExternalExcludeFilter($field, $table, $ext_target, $ext_table, $ext_key, $ext_value, $mysql_link){
 	$query="select $ext_target from $ext_table where $ext_key='$ext_value'";
 	$result = mysql_query($query, $mysql_link);
+If(mysql_num_rows($result)>0){
 	while($row = mysql_fetch_array($result)) {
 		If (!isset($first_pass_complete)) $temp = "(".$table.".".$field."!='".$row[$ext_target]."'";
 		If (isset($first_pass_complete)) $temp .= " AND ".$table.".".$field."!='".$row[$ext_target]."'";
 		$first_pass_complete = 1;
 	}
 $passes = $temp.")";
+} else $passes = "";
 return $passes;
 }
 
 function ExternalMinimumFilter($field, $table, $ext_target, $ext_table, $ext_key, $ext_value, $mysql_link){
 	$query="select $ext_target from $ext_table group by $ext_target having $ext_key>$ext_value";
 	$result = mysql_query($query, $mysql_link);
+If(mysql_num_rows($result)>0){
 	while($row = mysql_fetch_array($result)) {
 		If (!isset($first_pass_complete)) $temp = "(".$table.".".$field."='".$row[$ext_target]."'";
 		If (isset($first_pass_complete)) $temp .= " OR ".$table.".".$field."='".$row[$ext_target]."'";
 		$first_pass_complete = 1;
 	}
 $passes = $temp.")";
+} else $passes = "";
 return $passes;
 }
 
 function ExternalMaximumFilter($field, $table, $ext_target, $ext_table, $ext_key, $ext_value, $mysql_link){
 	$query="select $ext_target from $ext_table group by $ext_target having $ext_key<$ext_value";
 	$result = mysql_query($query, $mysql_link);
+If(mysql_num_rows($result)>0){
 	while($row = mysql_fetch_array($result)) {
 		If (!isset($first_pass_complete)) $temp = "(".$table.".".$field."='".$row[$ext_target]."'";
 		If (isset($first_pass_complete)) $temp .= " OR ".$table.".".$field."='".$row[$ext_target]."'";
 		$first_pass_complete = 1;
 	}
 $passes = $temp.")";
+} else $passes = "";
 return $passes;
 }
 
