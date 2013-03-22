1978f175c39b3186a608885cad421980419bd9c7
jkennaly <jkennaly@gmail.com>
2013-03-22 14:31:22 -0400

    debug pics

--- includes/content/blocks/getPicture.php
+++ includes/content/blocks/getPicture.php
@@ -1,16 +1,19 @@
 <?php
 session_start();
 
-echo $_SESSION['fest'];
+
+
+$master = mysql_connect($dbhost,$master_dbuser,$master_dbpw);
+@mysql_select_db($master_db, $master) or die( "Unable to select master database");
 
 include("../../../variables/variables.php");
 
 include('../../../variables/fest_variables.php');
 
 //echo "dbhost = $dbhost,dbuser = $dbuser,dbpw = $dbpw, dbname=$dbname";
-
 $main = mysql_connect($dbhost,$dbuser,$dbpw);
 @mysql_select_db($dbname, $main) or die( "Unable to select main database");
+
 
 $band=$_GET['band'];
 $sql = "SELECT `pic`, `type`, `descrip` FROM `pics` WHERE `band` = '$band' order by rand() limit 1";
