2f22817effc9f1649796ed43b08d8abb0f70f09c
jkennaly <jkennaly@gmail.com>
2013-03-22 14:48:38 -0400

    debug pics

--- includes/content/blocks/getPicture.php
+++ includes/content/blocks/getPicture.php
@@ -19,11 +19,11 @@ $sql = "SELECT `pic`, `type`, `descrip` FROM `pics` WHERE `band` = '$band' order
 $res = mysql_query($sql, $main);
 $pic = mysql_fetch_array($res);
 $picData = $pic['pic'];
-echo "Content-type: ".$pic['type'];
+//echo "Content-type: ".$pic['type'];
 //echo "Content-descrip: ".$pic['descrip'];
 
-//header("Content-type: ".$pic['type']);
-//echo $picData;
+header("Content-type: ".$pic['type']);
+echo $picData;
 
 mysql_close($main);
 mysql_close($master);
