32e79515808b955c733a2b20bb6fc5d08f442f41
jkennaly <jkennaly@gmail.com>
2013-03-22 15:13:23 -0400

    updated gametime to new master db scheme

--- mobile/mobile.php
+++ mobile/mobile.php
@@ -13,18 +13,34 @@
 
 <?php 
 
-include("../variables/variables.php");
+session_start(); 
 
-$main = mysql_connect($dbhost,$dbuser,$dbpw);
+include('../variables/variables.php');
+
 $master = mysql_connect($dbhost,$master_dbuser,$master_dbpw);
-@mysql_select_db($dbname, $main) or die( "Unable to select main database");
 @mysql_select_db($master_db, $master) or die( "Unable to select master database");
 
+function isInteger($input){
+    return(ctype_digit(strval($input)));
+}
 
- session_start(); 
- include('../variables/page_variables.php');  
- include('../includes/check_rights.php');  
- include('../includes/content/blocks/database_functions.php');
+If(!empty($_GET['fest']) && isInteger($_GET['fest'])) {
+	$_SESSION['fest'] = $_GET['fest'];
+} 
+ include('../includes/check_rights.php');   
+
+If(!empty($_SESSION['fest'])){
+
+include('../variables/fest_variables.php');
+//	echo "host=$dbhost user=$master_dbuser2 pw=$master_dbpw2 dbname=$dbname sitename =$sitename<br />";
+
+$main = mysql_connect($dbhost,$dbuser,$dbpw);
+@mysql_select_db($dbname, $main) or die( "Unable to select main database");
+
+
+ include('../variables/page_variables.php'); 
+ include('../includes/content/blocks/database_functions.php'); 
+}
 
 $right_required = "ViewNotes";
 If(isset($_SESSION['level']) && CheckRights($_SESSION['level'], $right_required)){
