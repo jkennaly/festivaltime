645b53f907f51c149e4681406fd0782bded71250
jkennaly <jkennaly@gmail.com>
2013-03-22 13:40:28 -0400

    debug fest variables

--- variables/fest_variables.php
+++ variables/fest_variables.php
@@ -1,8 +1,8 @@
 
 <?php
 //This page pulls the fest-specific data from the appropriate info table
-If(!empty($fest)){
-	$sql="select * from info_$fest";
+If(!empty($_SESSION['fest'])){
+	$sql="select * from info_".$_SESSION['fest'];
 	$result = mysql_query($sql, $master);
 	while($row=mysql_fetch_array($result)) {
 		switch($row['item']) {
