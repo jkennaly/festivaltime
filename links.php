d32a9833297e29403cf7f5dcfde8dc5c9d58653a
jkennaly <jkennaly@gmail.com>
2013-03-22 23:40:16 -0400

    fixed error in link logic

--- links.php
+++ links.php
@@ -8,21 +8,20 @@ If(isset($_SESSION['level']) && CheckRights($_SESSION['level'], $right_required)
 
 $link = htmlspecialchars($_GET["linkid"]);
 
-	mysql_connect($dbhost,$dbuser,$dbpw);
-	@mysql_select_db($dbname) or die( "Unable to select database");
+	$main=mysql_connect($dbhost,$dbuser,$dbpw);
+	@mysql_select_db($dbname, $main) or die( "Unable to select database");
 
 
 	$query="SELECT * FROM links WHERE id='$link'";
-	$query_link = mysql_query($query);
+	$query_link = mysql_query($query, $main);
 	$link_row = mysql_fetch_assoc($query_link);
 	$destination = $link_row["link"];
 
 	$clicker = "UPDATE links SET clicks=clicks+1 WHERE id='$link'";
-	$query = mysql_query($clicker);
+	$query = mysql_query($clicker, $main);
 
 	header("Location: $destination");
 
-mysql_close();
 }
 
 ?>
