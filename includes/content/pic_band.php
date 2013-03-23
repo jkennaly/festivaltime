fccf60121d5501a87532cfcfa5fb820eaa98084d
jkennaly <jkennaly@gmail.com>
2013-03-23 13:57:37 -0400

    debug

--- includes/content/pic_band.php
+++ includes/content/pic_band.php
@@ -44,7 +44,7 @@ This page allows for adding a picture of a band.
 
 include $baseinstall."includes/content/blocks/band_info.php";
 
-$link = "<a href=\"http://www.google.com/search?q=".str_replace(" ", "%20", $name)."&tbm=isch\" target=\"_blank\">Search Google for band pics</a>";
+echo "<a href=\"http://www.google.com/search?q=".str_replace(" ", "%20", $name)."&tbm=isch\" target=\"_blank\">Search Google for band pics</a>";
 
 ?>
 <form action="<?php echo $basepage."?disp=pic_band&band=".$band; ?>" method="post"
