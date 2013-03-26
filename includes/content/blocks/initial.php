d0ce7512cb44f2e715592f5aaf39bea5ec7e6b5e
jkennaly <jkennaly@gmail.com>
2013-03-26 11:42:44 -0400

    debug

--- includes/content/blocks/initial.php
+++ includes/content/blocks/initial.php
@@ -26,7 +26,7 @@ while($row=mysql_fetch_array($res)) {
 	$genredisp = "<table class=\"bandcap\"><caption align=\"bottom\">".$row['name']."<br />".getBandGenre($main, $master, $row['id'], $user)."</caption><tr><td class=\"pic_cell\"><a class=\"pic_row_pic\" href=\"".$basepage."?disp=view_band&band=".$row['id']."\"><img src=\"".$basepage."includes/content/blocks/getPicture.php?band=".$row['id']."&fest=".$_SESSION['fest']."\" alt=\"band pic\" /></a></td></tr></table>";
 	
 	echo $genredisp;
-	If($j % 3 == 0) echo "<br />";
+	If($j % 3 == 0) echo "<div class=\"clearfloat\"></div>";
 	$j++;
 } //Closes while($row=mysql_fetch_array($res))
 
