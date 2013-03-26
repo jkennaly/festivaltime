b08f498b1bca6cc7b6c6312fa19fd838520956f6
jkennaly <jkennaly@gmail.com>
2013-03-26 14:15:28 -0400

    debug

--- includes/content/blocks/pregame_band_activity2.php
+++ includes/content/blocks/pregame_band_activity2.php
@@ -10,7 +10,7 @@ If(mysql_num_rows($res)>0) {
 	$sql = "select d.id, d.response as reply, d.created as time, d.user as user from $discuss_table as d";
 	$res1 = mysql_query($sql, $main);
 	If(mysql_num_rows($res1)>0) {
-		echo"<h3><a id=\"displayText$user\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$row['user']."', 'displayText".$row['user']."');return false;\">show discussion</a>".$row['comment']."</h3><div id=\"toggleText$user\" style=\"display: none;\">";
+		echo"<h3><a id=\"displayText$user\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$user."', 'displayText".$user."');return false;\">show discussion</a>".$row['comment']."</h3><div id=\"toggleText$user\" style=\"display: none;\">";
 		while($row = mysql_fetch_array($res1)) {
 			echo "<p class=\"responder\">".getUname($master, $row['user'])." at ".$row['time']."<p><p id=\"reply\">".$row['reply']."</p>";
 		} //Closes while($row = mysql_fetch_array($res))
@@ -33,9 +33,11 @@ If(mysql_num_rows($res)>0) {
 	$sql = "select d.id, d.response as reply, d.created as time, d.user as user from $discuss_table as d";
 	$res1 = mysql_query($sql, $main);
 	If(mysql_num_rows($res1)>0) {
-		echo"<h3><a id=\"displayText".$row['user']."\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$row['user']."', 'displayText".$row['user']."');return false;\">show discussion</a>".$row['comment']."</h3><div id=\"toggleText".$row['user']."\" style=\"display: none;\">";
+		$i=0;
 		while($row = mysql_fetch_array($res1)) {
-			echo "<p class=\"responder\">".getUname($master, $row['user'])." at ".$row['time']."<p><p id=\"reply\">".$row['reply']."</p>";
+			If($i==0) echo"<h3><a id=\"displayText".$row['user']."\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$row['user']."', 'displayText".$row['user']."');return false;\">show discussion</a>".$row['comment']."</h3><div id=\"toggleText".$row['user']."\" style=\"display: none;\">";
+			echo "<p class=\"responder\">".getUname($master, $row['user'])." at ".$row['time']."<p><p>".$row['reply']."</p>";
+			$i++;
 		} //Closes while($row = mysql_fetch_array($res))
 		echo "</div>";
 	} else echo "<h3>".$row['comment']."</h3>";
