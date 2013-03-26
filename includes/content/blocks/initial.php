59f1cf11e7c3bb550cb01674c035e7539a31e057
jkennaly <jkennaly@gmail.com>
2013-03-26 11:40:12 -0400

    limit of 3 bands per row

--- includes/content/blocks/initial.php
+++ includes/content/blocks/initial.php
@@ -20,12 +20,14 @@ $res=mysql_query($sql, $main);
 
 //For each genre, find 3 bands to display
 //$j is how many genres have been displayed
-$j=0;
+$j=1;
 
 while($row=mysql_fetch_array($res)) {
 	$genredisp = "<table class=\"bandcap\"><caption align=\"bottom\">".$row['name']."<br />".getBandGenre($main, $master, $row['id'], $user)."</caption><tr><td class=\"pic_cell\"><a class=\"pic_row_pic\" href=\"".$basepage."?disp=view_band&band=".$row['id']."\"><img src=\"".$basepage."includes/content/blocks/getPicture.php?band=".$row['id']."&fest=".$_SESSION['fest']."\" alt=\"band pic\" /></a></td></tr></table>";
 	
 	echo $genredisp;
+	If($j % 3 == 0) echo "<br />";
+	$j++;
 } //Closes while($row=mysql_fetch_array($res))
 
 
