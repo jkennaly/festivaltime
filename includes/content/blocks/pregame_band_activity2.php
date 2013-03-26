b80a3b14eab606d398b030969f39418036d8e951
jkennaly <jkennaly@gmail.com>
2013-03-26 13:25:59 -0400

    test new comment/discussion display

---
+++ includes/content/blocks/pregame_band_activity2.php
@@ -0,0 +1,25 @@
+<?php
+
+//Get current users comment, if they have one
+$sql = "select comment from comments where user='$user' and band='$band'";
+$res=mysql_query($sql, $main);
+
+If(mysql_num_rows($res)>0) {
+	$row=mysql_fetch_array($res); 
+	echo"<h3><a id=\"displayText\" href=\"javascript:toggle(toggleText, displayText);\">show discussion</a>".$row['comment']."</h3><div id=\"toggleText\" style=\"display: none\">";
+	$sql = "select d.id, d.response as reply, d.created as time, d.user as user from $discuss_table as d";
+$res = mysql_query($sql, $main);
+while($row = mysql_fetch_array($res)) {
+	echo "<p class=\"responder\">".getUname($master, $row['user'])." at ".$row['time']."<p><p id=\"reply\">".$row['reply']."</p>";
+} //Closes while($row = mysql_fetch_array($res))
+	echo "</div>";
+}
+//Get user comments from group members
+
+
+
+
+//Get all other user comments
+
+
+?>
