9f8b2ed30ccbb736c66ec67fd24152d193793c40
jkennaly <jkennaly@gmail.com>
2013-03-26 19:02:23 -0400

    Distinuguished between new and old discussions from pregame

--- includes/content/blocks/pregame_band_activity2.php
+++ includes/content/blocks/pregame_band_activity2.php
@@ -6,12 +6,23 @@ $res=mysql_query($sql, $main);
 
 If(mysql_num_rows($res)>0) {
 	$row=mysql_fetch_array($res); 
+	
+			
+	//Check to see if the user is current on this discussion. 
+	$sqlcurrent = "select id from comments where id='".$row['id']."' AND discuss_current LIKE '%--$user--%'";
+	$resultcurrent = mysql_query($sqlcurrent, $main);
+	//IF the user is not current on this discussion
+	If(mysql_num_rows($resultcurrent) == 0){
+		$stat = "New discussion!";
+	} else $stat = "Show discussion!";	
+	
+	
 	$discuss_table= "discussion_".$row['id'];
 	$sql = "select d.id, d.response as reply, d.created as time, d.user as user from $discuss_table as d";
 	$res1 = mysql_query($sql, $main);
 	$leftcell= "<h2>".getUname($master, $row['user'])."</h2>";
 	If(mysql_num_rows($res1)>0) {
-		$leftcell.="<a id=\"displayText$user\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$user."', 'displayText".$user."', $user, ".$row['comment'].");return false;\">show discussion</a>";
+		$leftcell.="<a id=\"displayText$user\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$user."', 'displayText".$user."', $user, ".$row['comment'].");return false;\">$stat</a>";
 		$rightcell = "<h3>".$row['comment']."</h3>";
 		$discuss ="<div id=\"toggleText$user\" style=\"display: none;\">";
 		while($row1 = mysql_fetch_array($res1)) {
@@ -37,26 +48,42 @@ $sql = "select id, comment, user from comments where user!='$user' and band='$ba
 $res=mysql_query($sql, $main);
 
 If(mysql_num_rows($res)>0) {
-	while($row=mysql_fetch_array($res)) { 
+	while($row=mysql_fetch_array($res)) {
+		
+		
+	//Check to see if the user is current on this discussion. 
+	$sqlcurrent = "select id from comments where id='".$row['id']."' AND discuss_current LIKE '%--$user--%'";
+	$resultcurrent = mysql_query($sqlcurrent, $main);
+	//IF the user is not current on this discussion
+	If(mysql_num_rows($resultcurrent) == 0){
+		$stat = "New discussion!";
+	} else $stat = "Show discussion!";
+
+ 
 		$discuss_table= "discussion_".$row['id'];
 		$sql = "select d.id, d.response as reply, d.created as time, d.user as user from $discuss_table as d";
 		$res1 = mysql_query($sql, $main);
-					echo "<h2><br />".getUname($master, $row['user'])." ".displayStars($band, $row['user'], $main, "displaystars", $basepage."includes/images")."</h2>";
+			$leftcell= "<h2><br />".getUname($master, $row['user'])."<br />".displayStars($band, $row['user'], $main, "displaystars", $basepage."includes/images")."</h2>";
 		If(mysql_num_rows($res1)>0) {
 			$i=0;
 			while($row1 = mysql_fetch_array($res1)) {
 				If($i==0) {
-					echo"<h3><a id=\"displayText".$row['user']."\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$row['user']."', 'displayText".$row['user']."', '".$row['user']."', '".$row['comment']."');return false;\">show discussion</a>".$row['comment']."</h3><div id=\"toggleText".$row['user']."\" style=\"display: none;\">";
+					$leftcell.="<a id=\"displayText".$row['user']."\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$row['user']."', 'displayText".$row['user']."', '".$row['user']."', '".$row['comment']."');return false;\">$stat</a>";
+					$rightcell = "<h3>".$row['comment']."</h3>";
+					$discuss = "<div id=\"toggleText".$row['user']."\" style=\"display: none;\">";
 				}
-				echo "<p class=\"responder\">".getUname($master, $row1['user'])." at ".$row1['time']."<p><p>".$row1['reply']."</p>";
+				$discuss .= "<p class=\"responder\">".getUname($master, $row1['user'])." at ".$row1['time']."<p><p>".$row1['reply']."</p>";
 				$i++;
 			} //Closes while($row = mysql_fetch_array($res))
-			echo "<br /><a href=\"$basepage?disp=discussion&comment=".$row['id']."\">Reply to this discussion</a></div>";
+			$discuss .= "<br /><a href=\"$basepage?disp=discussion&comment=".$row['id']."\">Reply to this discussion</a></div>";
 		} else  {
-			echo "<div><h3>".$row['comment']."</h3>";
-			echo "<a href=\"$basepage?disp=discussion&comment=".$row['id']."\">Start a discussion</a></div>";
+			$rightcell = "<div><h3>".$row['comment']."</h3>";
+			$leftcell.= "<a href=\"$basepage?disp=discussion&comment=".$row['id']."\">Start a discussion</a></div>";
+			$discuss="";
 		}
 	}
+	echo "<table class=\"commentstable\" id=\"comment".$row['user']."\"><tr><th>$leftcell</th><td>$rightcell</td></tr></table>";
+	echo $discuss;
 }
 
 ?>
