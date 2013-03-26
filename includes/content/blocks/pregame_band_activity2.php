b0a239025d6530e383bbe2154ace6b741fa6d38d
jkennaly <jkennaly@gmail.com>
2013-03-26 15:25:56 -0400

    debug

--- includes/content/blocks/pregame_band_activity2.php
+++ includes/content/blocks/pregame_band_activity2.php
@@ -13,7 +13,7 @@ If(mysql_num_rows($res)>0) {
 	If(mysql_num_rows($res1)>0) {
 		echo"<h3><a id=\"displayText$user\" title=\"Click to toggle discussion\" href=\"#\" onclick=\"toggle('toggleText".$user."', 'displayText".$user."');return false;\">show discussion</a>".$row['comment']."</h3><div id=\"toggleText$user\" style=\"display: none;\">";
 		while($row1 = mysql_fetch_array($res1)) {
-			echo "<p class=\"responder\">".getUname($master, $row['user'])." at ".$row1['time']."<p><p id=\"reply\">".$row1['reply']."</p>";
+			echo "<p class=\"responder\">".getUname($master, $row1['user'])." at ".$row1['time']."<p><p id=\"reply\">".$row1['reply']."</p>";
 		} //Closes while($row = mysql_fetch_array($res))
 		echo "<br /><a href=\"$basepage?disp=discussion&comment=".$row['id']."\">Reply to this discussion</a></div>";
 	} else {
