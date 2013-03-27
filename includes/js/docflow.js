8dfc2cced1f921207a0007b07ebdb14e7893b5a0
jkennaly <jkennaly@gmail.com>
2013-03-26 20:22:12 -0400

    autoscroll to reply button

--- includes/js/docflow.js
+++ includes/js/docflow.js
@@ -1,3 +1,27 @@
+function post_to_url(path, params, method) {
+    method = method || "post"; // Set method to post by default, if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+
 function toggle(divid, aid, user, comment, scrollid) {
 	var ele = document.getElementById(divid);
 	var text = document.getElementById(aid);
@@ -9,6 +33,6 @@ function toggle(divid, aid, user, comment, scrollid) {
 		ele.style.display = "block";
 		text.innerHTML = "hide";
 	}
-	$.post("includes/php/update_discussion.php", { user: user, comment: comment });
+	post_to_url("includes/php/update_discussion.php", { user: user, comment: comment }, 'post');
 	document.getElementById(scrollid).scrollIntoView();
 }
