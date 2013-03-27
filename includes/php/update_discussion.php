d1d50073db55c6587d3d8ebb7fa33f60b212979f
jkennaly <jkennaly@gmail.com>
2013-03-26 21:40:03 -0400

    upd

--- includes/php/update_discussion.php
+++ includes/php/update_discussion.php
@@ -1,7 +1,36 @@
 <?php
 
+session_start(); 
+
+include('variables/variables.php');
+
+$master = mysql_connect($dbhost,$master_dbuser,$master_dbpw);
+@mysql_select_db($master_db, $master) or die( "Unable to select master database");
+
+function isInteger($input){
+    return(ctype_digit(strval($input)));
+}
+
+If(isset($_GET['fest']) && isInteger($_GET['fest'])) {
+	$_SESSION['fest'] = $_GET['fest'];
+} 
+ include('includes/check_rights.php');
+ include('includes/content/blocks/database_functions.php'); 
+include('includes/content/blocks/other_functions.php'); 
+
+
+If(!empty($_SESSION['fest'])){
+
+include('variables/fest_variables.php');
+//	echo "host=$dbhost user=$master_dbuser2 pw=$master_dbpw2 dbname=$dbname sitename =$sitename<br />";
+
+$main = mysql_connect($dbhost,$dbuser,$dbpw);
+@mysql_select_db($dbname, $main) or die( "Unable to select main database");
+}
+
 $user = $_POST['user'];
 $comment = $_POST['comment'];
+
 
 
 //Check to see if the user is current on this discussion. If not, make it happen
