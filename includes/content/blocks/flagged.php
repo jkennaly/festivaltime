3e9ca5ef9c3556a618ccf632ba6822060815b0a2
jkennaly <jkennaly@gmail.com>
2013-03-27 10:07:18 -0400

    clarified what flagged pics are

--- includes/content/blocks/flagged.php
+++ includes/content/blocks/flagged.php
@@ -10,6 +10,7 @@ If(isset($_SESSION['level']) && !empty($user) && CheckRights($_SESSION['level'],
 $query="select * from pics where clicks>0";
 $result = mysql_query($query, $main);
 If(mysql_num_rows($result)>0){
+    echo "<h2>The following pictures have been flagged for removal:</h2>";
     while($row=mysql_fetch_array($result)){
         echo "<a class=\"pic_row_pic\" href=\"".$basepage."?disp=band_gallery&band=".$row['band']."\">";
         echo "<img src=\"".$basepage."includes/content/blocks/getSpecPic.php?picid=".$row['id']."&fest=".$_SESSION['fest']."\" alt=\"flagged pic\" /></a>";
