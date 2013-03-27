a4b8a44805f458777343684c485c1c7562b137c2
jkennaly <jkennaly@gmail.com>
2013-03-26 21:34:43 -0400

    fixed error preventing login

--- includes/unselected.php
+++ includes/unselected.php
@@ -13,6 +13,8 @@ chdir($old_path);
 	//If there is a specific type of content requested, and there is a file with that name, display it
 	If(in_array($disp . ".php", $content_files)){
 		include $baseinstall."includes/unselected/" . $disp . ".php";
+	} elseif($disp == "login") {
+		include $baseinstall."includes/content/" . $disp . ".php";
 	}
 	//If a content file is requested that does not exist, return error
 	else{
