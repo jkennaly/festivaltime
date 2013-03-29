9bf20578993508c0d367618ac4c30def44e322a2
jkennaly <jkennaly@gmail.com>
2013-03-29 18:36:20 -0400

    expanded email test

--- includes/content/email_test.php
+++ includes/content/email_test.php
@@ -1,6 +1,6 @@
 <?php
- $to = "jkennaly@gmail.com";
- $headers = 'From: Festival Time <festivaltime.us@gmail.com>';
+ $to = "matthew.gluth@me.com, egeigle@gmail.com";
+ $headers = 'From: Festival Time <festivaltime.us@gmail.com>' . "\r\n" . "Cc: jkennaly@gmail.com";
  $subject = "Email Test";
  $body = "This is a test. Please begin to panic now.";
 mail($to, $subject, $body, $headers);
