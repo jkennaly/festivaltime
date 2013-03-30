5bbd3b6961ebb4f7cabb1c77ac76f284e2eb225d
jkennaly <jkennaly@gmail.com>
2013-03-30 00:39:15 -0400

    another attempt at copyright headers

--- includes/content/login.php
+++ includes/content/login.php
@@ -1,16 +1,14 @@
-
-
-
-
 <?php
-
-if(!empty($_POST)){ session_destroy(); Login($master);}
-if(!empty($_SESSION['user'])){
-if(empty($_POST)) echo "User $uname already logged in. Press Log Out first to change user."; 
-include "home.php";
-}
-else{
+//Copyright (c) 2013 Jason Kennaly.
+//All rights reserved. This program and the accompanying materials
+//are made available under the terms of the GNU Affero General Public License v3.0 which accompanies this distribution, and is available at
+//http://www.gnu.org/licenses/agpl.html
+//
+//Contributors:
+//    Jason Kennaly - initial API and implementation
 ?>
+
+
 <div id="content">
 <form id='login' action='index.php?disp=login' method='post' accept-charset='UTF-8'>
 <fieldset >
