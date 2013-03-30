900e56261b078035449eb4fb594f88d8a30bacc7
jkennaly <jkennaly@gmail.com>
2013-03-30 00:27:49 -0400

    updating install instructions/fixing bad header info

--- includes/content/blocks/filter_process.php
+++ includes/content/blocks/filter_process.php
@@ -1,12 +1,4 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2013 Jason Kennaly.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Affero General Public License v3.0 which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/agpl.html
-# 
-# Contributors:
-#     Jason Kennaly - initial API and implementation
-#-------------------------------------------------------------------------------
+
 <?php
 If(!empty($_POST['new']) || !empty($_POST['all_bands'])) {
 unset($_SESSION['filter']);
