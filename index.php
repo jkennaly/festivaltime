c54e4daffe8b99b05ff5b89dc01549c99d9f1666
jkennaly <jkennaly@gmail.com>
2013-03-30 00:35:13 -0400

    updated license info

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+/*
+*Copyright (c) 2013 Jason Kennaly.
+*All rights reserved. This program and the accompanying materials
+*are made available under the terms of the GNU Affero General Public License v3.0 which accompanies this distribution, and is available at
+*http://www.gnu.org/licenses/agpl.html
+*
+*Contributors:
+*    Jason Kennaly - initial API and implementation
+*/
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
