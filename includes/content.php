c1133d0a42cc3e5ebced92798692e77fc17f7eed
jkennaly <jkennaly@gmail.com>
2013-03-30 02:02:57 -0400

    created new class of pages that are available whetehr or not a fest is selected, called site. Added login and logout

--- includes/content.php
+++ includes/content.php
@@ -17,13 +17,17 @@ If(empty($_GET["disp"])) $disp = "home";
 $old_path = getcwd();
 chdir($baseinstall."includes/content/");
 $content_files = glob("*.php");
+chdir($baseinstall."includes/site/");
+$site_files = glob("*.php");
 chdir($old_path);
 
 
 	//If there is a specific type of content requested, and there is a file with that name, display it
 	If(in_array($disp . ".php", $content_files)){
 		include $baseinstall."includes/content/" . $disp . ".php";
-	}
+	} elseif(in_array($disp . ".php", $site_files)){
+        include $baseinstall."includes/site/" . $disp . ".php";
+    }
 	//If a content file is requested that does not exist, return error
 	else{
 		include $baseinstall."includes/content/error.php";
--- includes/unselected.php
+++ includes/unselected.php
@@ -17,15 +17,17 @@ If(empty($_GET["disp"]) || $disp ==  "home") $disp = "fest_select";
 $old_path = getcwd();
 chdir($baseinstall."includes/unselected/");
 $content_files = glob("*.php");
+chdir($baseinstall."includes/site/");
+$site_files = glob("*.php");
 chdir($old_path);
 
 
 	//If there is a specific type of content requested, and there is a file with that name, display it
 	If(in_array($disp . ".php", $content_files)){
 		include $baseinstall."includes/unselected/" . $disp . ".php";
-	} elseif($disp == "login") {
-		include $baseinstall."includes/content/" . $disp . ".php";
-	}
+	} elseif(in_array($disp . ".php", $site_files)){
+        include $baseinstall."includes/site/" . $disp . ".php";
+    }
 	//If a content file is requested that does not exist, return error
 	else{
 		include $baseinstall."includes/content/error.php";
