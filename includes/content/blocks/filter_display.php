63b3fc416451d248083ca1140c535c743cee7919
jkennaly <jkennaly@gmail.com>
2013-03-30 00:37:56 -0400

    Revert "updated license info"
    This reverts commit c54e4daffe8b99b05ff5b89dc01549c99d9f1666.

--- includes/content/blocks/filter_display.php
+++ includes/content/blocks/filter_display.php
@@ -1,14 +1,3 @@
-/*
-*Copyright (c) 2013 Jason Kennaly.
-*All rights reserved. This program and the accompanying materials
-*are made available under the terms of the GNU Affero General Public License v3.0 which accompanies this distribution, and is available at
-*http://www.gnu.org/licenses/agpl.html
-*
-*Contributors:
-*    Jason Kennaly - initial API and implementation
-*/
-
-
 <div id="filter">
 
 <?php
