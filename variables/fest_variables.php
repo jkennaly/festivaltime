a33d824320a3ecb675ee66748a51096f22870975
jkennaly <jkennaly@gmail.com>
2013-03-29 23:33:47 -0400

    Included AGPL license info/copyright in source

--- variables/fest_variables.php
+++ variables/fest_variables.php
@@ -1,3 +1,12 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2013 Jason Kennaly.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the GNU Affero General Public License v3.0 which accompanies this distribution, and is available at
+# http://www.gnu.org/licenses/agpl.html
+# 
+# Contributors:
+#     Jason Kennaly - initial API and implementation
+#-------------------------------------------------------------------------------
 <?php
 //This page pulls the fest-specific data from the appropriate info table
 If(!empty($_SESSION['fest'])){ $fest = $_SESSION['fest'];}
